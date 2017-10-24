bbe9ffac83644f3363164f9f92ffb2f4bcebb9b7
Taha Dogan Gunes <tdgunes@gmail.com>
2017-10-24 18:01:42 +0100

    Add discount factor and reservation prints

--- src/ExampleAgent.java
+++ src/ExampleAgent.java
@@ -22,6 +22,10 @@ public class ExampleAgent extends AbstractNegotiationParty {
     @Override
     public void init(NegotiationInfo info) {
         super.init(info);
+
+        System.out.println("Discount Factor is " + info.getUtilitySpace().getDiscountFactor());
+        System.out.println("Reservation Value is " + info.getUtilitySpace().getReservationValueUndiscounted());
+
     }
 
     /**
